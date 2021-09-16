f6c58196355df294bb2984e89668185fc7463f2b
barnold8 <bandslover@outlook.con>
2021-09-16 15:23:13 +0100

    Adding OS support update

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 from random import randint
 import os
+import platform
+
+
+def openLink(a,b,x1,x2,x3,x4):
+
+  system = platform.system()
+
+  openType = "start" if system == "Windows" else "xdg-open" 
+
+  string = "{} https://prnt.sc/{}{}{}{}{}{}".format(openType,a,b,x1,x2,x3,x4)
+  os.system(string)
+
 
 for i in range(int(input("How many random images do you want to see?"))):
   x1 = randint(0,9)
@@ -8,6 +20,5 @@ for i in range(int(input("How many random images do you want to see?"))):
   x4 = randint(0,9)
   a = chr(randint(97,122))
   b = chr(randint(97,122))
-  string = "start https://prnt.sc/{}{}{}{}{}{}".format(a,b,x1,x2,x3,x4)
-  os.system(string)
+  openLink(a,b,x1,x2,x3,x4)
   #print(string)
